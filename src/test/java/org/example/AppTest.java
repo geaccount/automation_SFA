c4d4e849925b546970d2934c943588e8c295b3e1
Raka F <raka@eskalink.com>
2024-06-18 22:28:24 +0700

    add file setup pom, and run test

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,38 +1,47 @@
 package org.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        tags = "@TestRun",
+        features = {"src/test/resources/features"},
+        glue = {"org.example","com.selenium.stepDefinitions"},
+        plugin = {"pretty",
+                "html:target/report/cucumber-report.html",
+                "json:target/report/cucumber-report.json",
+                "json:target/jsonReports/reports/cucumber.json"
+        }
+)
+public class AppTest {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+//    public AppTest( String testName )
+//    {
+//        super( testName );
+//    }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+//    public static Test suite()
+//    {
+//        return new TestSuite( AppTest.class );
+//    }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+//    public void testApp()
+//    {
+//        assertTrue( true );
+//    }
 }
